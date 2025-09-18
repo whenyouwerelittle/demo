c7a2ab23c3f3f662ce1837a8836d1e9e2f921367
whenyouwerelittle <whenyouwerelittle@gmail.com>
2025-09-18 15:45:05 +0900

    setup entity, service, dto, repository and global/jpa/BaseEntity

---
+++ src/main/java/com/example/demo/article/entity/Article.java
@@ -0,0 +1,19 @@
+package com.example.demo.article.entity;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class Article {
+    private String subject;
+    private String content;
+
+
+}
---
+++ src/main/java/com/example/demo/article/service/ArticleService.java
@@ -0,0 +1,7 @@
+package com.example.demo.article.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleService {
+}
