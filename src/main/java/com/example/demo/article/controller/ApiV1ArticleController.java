43f3621adb2575682dc48b6bd6e58052760af12e
whenyouwerelittle <whenyouwerelittle@gmail.com>
2025-09-19 14:21:07 +0900

    article api v1

---
+++ src/main/java/com/example/demo/article/controller/ApiV1ArticleController.java
@@ -0,0 +1,38 @@
+package com.example.demo.article.controller;
+
+import com.example.demo.article.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController //responsebody 없이 json 주고받음
+// @RestControllerAdvice
+@RequestMapping("api/v1/articles")
+@RequiredArgsConstructor // 필드 자동생성
+public class ApiV1ArticleController {
+    private final ArticleService articleService;
+
+    @GetMapping("")
+    public String list(){
+        return "lists";
+    }
+
+    @GetMapping("/{id}")
+    public String getArticle(){
+        return "single";
+    }
+
+    @PostMapping("")
+    public String create(){
+        return "create";
+    }
+
+    @PatchMapping("/{id}")
+    public String modify(){
+        return "modify";
+    }
+
+    @DeleteMapping()
+    public String delete(){
+        return "delete";
+    }
+}
