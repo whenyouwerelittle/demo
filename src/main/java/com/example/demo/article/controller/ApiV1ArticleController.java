1c0e4ca3b92e1d6cd714bdff0fa1159833969f6b
whenyouwerelittle <whenyouwerelittle@gmail.com>
2025-09-19 15:44:34 +0900

    수정삭제를 제외한 ëª목록 단일 전시 를 postman 과 시험함

--- src/main/java/com/example/demo/article/controller/ApiV1ArticleController.java
+++ src/main/java/com/example/demo/article/controller/ApiV1ArticleController.java
@@ -1,38 +1,64 @@
 package com.example.demo.article.controller;
 
+import com.example.demo.article.dto.ArticleDTO;
+import com.example.demo.article.entity.Article;
 import com.example.demo.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController //responsebody 없이 json 주고받음
 // @RestControllerAdvice
-@RequestMapping("api/v1/articles")
-@RequiredArgsConstructor // 필드 자동생성
+@RequestMapping("/api/v1/articles")
+@RequiredArgsConstructor
 public class ApiV1ArticleController {
     private final ArticleService articleService;
 
     @GetMapping("")
-    public String list(){
-        return "lists";
+    public List<ArticleDTO> list() {
+        List<ArticleDTO> articleList = new ArrayList<>();
+
+        Article article1 = new Article("제목 1", "내용1");
+        articleList.add(new ArticleDTO(article1));
+
+        Article article2 = new Article("제목 2", "내용2");
+        articleList.add(new ArticleDTO(article2));
+
+        Article article3 = new Article("제목 3", "내용3");
+        articleList.add(new ArticleDTO(article3));
+
+        return articleList;
     }
 
     @GetMapping("/{id}")
-    public String getArticle(){
-        return "single";
+    public ArticleDTO getArticle(@PathVariable("id") Long id) {
+        Article article = new Article("제목1", "내용1");
+
+        ArticleDTO articleDTO = new ArticleDTO(article);
+
+        return articleDTO;
     }
 
     @PostMapping("")
-    public String create(){
-        return "create";
+    public String create(@RequestParam("subject") String subject, @RequestParam("content") String content) {
+        System.out.println(subject);
+        System.out.println(content);
+        return "등록";
     }
 
     @PatchMapping("/{id}")
-    public String modify(){
-        return "modify";
+    public String modify(@PathVariable("id") Long id, @RequestParam("subject") String subject, @RequestParam("content") String content) {
+        System.out.println(id);
+        System.out.println(subject);
+        System.out.println(content);
+        return "수정";
     }
 
-    @DeleteMapping("/{id}")
-    public String delete(){
-        return "delete";
+    @DeleteMapping("{id}")
+    public String delete(@PathVariable("id") Long id) {
+        System.out.println(id);
+        return "삭제";
     }
 }
